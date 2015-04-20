b5c984db53268651ab46a3038c1e50394924bd2a
Jon Paul <codingfriend1@gmail.com>
2015-04-20 18:47:54 -0500

    Changes git ignore file to ignore images and updates some styles on image-selector. Adds open and close menu button.

--- client/app/cms/cms.controller.js
+++ client/app/cms/cms.controller.js
@@ -5,5 +5,11 @@
 	CMSCtrl.$inject = ['$scope', 'Auth'];
 	function CMSCtrl($scope, Auth) {
 		$scope.user = Auth.getCurrentUser();
+
+
+
+		$scope.toggleMenu = function() {
+			$scope.menuOpen = !$scope.menuOpen;
+		};
   }
 })();
