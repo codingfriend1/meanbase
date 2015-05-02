0c2758ad2b75dec200c430bbdc3f63f6fc5a11fd
Jon Paul <codingfriend1@gmail.com>
2015-05-01 19:36:02 -0500

    Makes endpoints services able to handle full urls and cleans up code.

--- client/components/endpoints/endpoints.service.js
+++ client/components/endpoints/endpoints.service.js
@@ -2,34 +2,41 @@
 	angular.module('meanbaseApp').factory('endpoints', ['$http', function($http) {
 
 		function endpoints(endpoint) {
-			this.baseRoute = '/api/';
-			this.endpoint = endpoint;
+			this.endpoint = endpoint || '';
+
+			// If the url is a full address then don't modify it
+			if(endpoint.indexOf('http://') > -1 || endpoint.indexOf('https://') > -1) {
+				this.url = endpoint;
+			} else {
+				// else prefix it with /api/ so it calls our server api
+				this.url = '/api/' + this.endpoint;
+			}
 		}
 
 		endpoints.prototype.create = function(data) {
 			var self = this;
-			return $http.post(this.baseRoute + this.endpoint, data).error(function(error) {
+			return $http.post(this.url, data).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
 
 		endpoints.prototype.find = function(identifier) {
 			var self = this;
-			return $http.get(this.baseRoute + this.endpoint, {params: identifier}).error(function(error) {
+			return $http.get(this.url, {params: identifier}).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
 
 		endpoints.prototype.update = function(identifier, replacement) {
 			var self = this;
-			return $http.put(this.baseRoute + this.endpoint, {identifier: identifier, replacement: replacement}).error(function(error) {
+			return $http.put(this.url, {identifier: identifier, replacement: replacement}).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
 
 		endpoints.prototype.delete = function(identifier) {
 			var self = this;
-			return $http.delete(this.baseRoute + this.endpoint, {
+			return $http.delete(this.url, {
 			    params: identifier,
 			    headers: {"Content-Type": "application/json;charset=utf-8"}
   			}).error(function(error) {
@@ -39,21 +46,21 @@
 
 		endpoints.prototype.findOne = function(id) {
 			var self = this;
-			return $http.get(this.baseRoute + this.endpoint + '/' + id).error(function(error) {
+			return $http.get(this.url + '/' + id).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
 
 		endpoints.prototype.updateOne = function(id, replacement) {
 			var self = this;
-			return $http.put(this.baseRoute + this.endpoint + '/' + id, replacement).error(function(error) {
+			return $http.put(this.url + '/' + id, replacement).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
 
 		endpoints.prototype.deleteOne = function(id) {
 			var self = this;
-			return $http.delete(this.baseRoute + this.endpoint + '/' + id).error(function(error) {
+			return $http.delete(this.url + '/' + id).error(function(error) {
 				self.errorHandler(error);
 			});
 		};
