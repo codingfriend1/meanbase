bd58286b5a5321238ecd003a1b080ec8c5e52b7d
Jon Paul <codingfriend1@gmail.com>
2015-05-10 14:29:42 -0500

    Renames search-form to searchForm so directive will compile.

--- client/extensions/search-form/search-form.directive.js
+++ client/extensions/search-form/search-form.directive.js
@@ -3,7 +3,7 @@
 // This directive uses the slug passed in to get the appropriate images and display them in a slider
 
 angular.module('extensions')
-  .directive('search-form', function (endpoints, $rootScope) {
+  .directive('searchForm', function (endpoints, $rootScope) {
     return {
       templateUrl: 'extensions/search-form/search-form.html',
       restrict: 'EA',
