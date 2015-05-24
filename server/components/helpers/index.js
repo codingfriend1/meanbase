5414124de3a78ba2b6df7e2b9faa937a825e29d0
Jon Paul <codingfriend1@gmail.com>
2015-05-24 16:30:17 -0500

    Moves retrieve themes into helpers file.

--- server/components/helpers/index.js
+++ server/components/helpers/index.js
@@ -41,3 +41,40 @@ exports.objectToArray = function(data) {
   }
   return returnArray;
 };
+
+var fs = require('fs');
+
+exports.retrieveThemes = function(activeURL, callback) {
+  var themesFolderUrl = 'client/themes/';
+  // Loop through themes in themesFolderUrl and get the theme.json file out of the root of each one
+  var themesFolder = fs.readdirSync(themesFolderUrl);
+  var themeJSONS = [];
+  var anyActive = false;
+
+  for(var file = 0; file < themesFolder.length; file++) {
+    if(themesFolder[file][0] !== '.') {
+      var stat = fs.statSync(themesFolderUrl + themesFolder[file]);
+      if(stat.isDirectory()) {
+        try {
+          var themeJSON = JSON.parse(fs.readFileSync(themesFolderUrl + themesFolder[file] + '/theme.json', 'utf8'));
+          if(themeJSON && Object.prototype.toString.call(themeJSON) === "[object Object]") {
+            themeJSON.url = themesFolder[file];
+            if(themeJSON.url === activeURL) {
+              anyActive = true;
+              themeJSON.active = true;
+            }
+            themeJSONS.push(themeJSON);
+          }
+        } catch(error) {
+          if(callback) { return callback('Could not find theme.json in root of theme'); }
+          console.log('Could not find theme.json in root of theme: ' + error);
+        }
+      }
+    }
+  } //for
+
+  // If no themes were already active make the first one retrieved active
+  if(!anyActive) { themeJSONS[0].active = true; }
+
+  return themeJSONS;
+};
--- server/init/themes.js
+++ server/init/themes.js
@@ -1,47 +1,15 @@
 var fs = require('fs');
+var Themes = require('../api/themes/themes.model');
+var helpers = require('../components/helpers');
 
 module.exports = function(callback) {
-	console.log('creating themes...');
-	var Themes = require('../api/themes/themes.model');
-	var themesFolderUrl = 'client/themes/';
-
+	console.log('Checking themes...');
 
 	// Loop through themes in themesFolderUrl and get the theme.json file out of the root of each one
-	var themesFolder = fs.readdirSync(themesFolderUrl);
-	var themeJSONS = [];
-	var anyActive = false;
 	Themes.find({active: true}).lean().exec(function (err, activeTheme) {
 	  if(err) { return handleError(err); }
-	  activeTheme = activeTheme[0];
-	  if(!activeTheme || !activeTheme.url) {
-	  	activeTheme.url = '';
-	  }
-
-		for(var file = 0; file < themesFolder.length; file++) {
-			if(themesFolder[file][0] !== '.') {
-				var stat = fs.statSync(themesFolderUrl + themesFolder[file]);
-				if(stat.isDirectory()) {
-					try {
-						var themeJSON = JSON.parse(fs.readFileSync(themesFolderUrl + themesFolder[file] + '/theme.json', 'utf8'));
-						if(themeJSON && Object.prototype.toString.call(themeJSON) === "[object Object]") {
-							themeJSON.url = themesFolder[file];
-							if(themeJSON.url === activeTheme.url) {
-								anyActive = true;
-								themeJSON.active = true;
-							}
-							themeJSONS.push(themeJSON);
-						}
-					} catch(error) {
-						if(callback) { return callback('Could not find theme.json in root of theme'); }
-						console.log('Could not find theme.json in root of theme: ' + error);
-					}
-				}
-			}
-		} //for
-
-
-		// Set the first theme active automatically just as something to start with
-		if(!anyActive) { themeJSONS[0].active = true; }
+	  if(!activeTheme[0] || !activeTheme[0].url) { activeTheme[0].url = ''; }
+	  var themeJSONS = helpers.retrieveThemes(activeTheme[0].url, callback);
 
 		// Remove all found themes
 		if(themeJSONS) {
@@ -49,31 +17,14 @@ module.exports = function(callback) {
 		 		if(err) { return handleError(err); }
 		  	Themes.create(themeJSONS, function(err, theme) {
 				  if(err) { return handleError(err); }
-				  if(callback) {
-				  	return callback(null);
-				  }
 				  console.log('themes initialized');
+				  if(callback) { return callback(null); }
 				});
 			});
 		}
 
 
 	}); //themes find
-
-	
-	
-
-	// Find all themes in database
-	// if(themeJSONS) {
-		// Themes.find(function (err, themes) {
-		//   if(err) { return handleError(err); }
-		//   if(themes.length > 0) { return false; }
-	 //  	Themes.create(themeJSONS, function(err, theme) {
-		// 	  if(err) { return handleError(err); }
-		// 	  console.log('themes initialized');
-		// 	});
-		// });
-	// }
 	
 
 	function handleError(err) {
