84c59219a546e1ab535f627290f2fd4334aa17c8
Jon Paul <codingfriend1@gmail.com>
2015-06-03 21:04:21 -0500

    Adds screenshots for different page templates in meanbase-starter and has archive controller pull all articles.

--- client/themes/meanbase-starter/templates/archive/archive.controller.js
+++ client/themes/meanbase-starter/templates/archive/archive.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('meanbaseApp')
-  .controller('ArchiveCtrl', function ($scope) {
-  	
+  .controller('ArchiveCtrl', function ($scope, endpoints) {
+  	var posts = new endpoints('pages');
+
+  	posts.find({template: 'article'}).success(function(response) {
+  		$scope.posts = response;
+  	});
   });
