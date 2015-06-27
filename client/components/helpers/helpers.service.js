842f21c07771a4e23d8529f611313e1f32dc16c4
Jon Paul <codingfriend1@gmail.com>
2015-06-27 12:18:04 -0500

    Finishes documentation for helpers.service.

--- client/components/helpers/helpers.service.js
+++ client/components/helpers/helpers.service.js
@@ -1,18 +1,28 @@
+// This service contains various helper functions used in common tasks all around the CMS
 'use strict';
 
 angular.module('meanbaseApp')
   .service('helpers', function ($rootScope) {
+
+    // ### Convert Array to Object with Array
+    // helpers.arrayToObjectWithArray() receives an array of objects and converts them into and object of arrays with the property name being the value of the string property that was passed into `itemToBecomeProperty`
     this.arrayToObjectWithArray = function(array, itemToBecomeProperty) {
       if(!array || !itemToBecomeProperty) { return array; }
       if(!Array.isArray(array)) { array = [array]; }
 
       var returnObject = {};
       for (var ii = 0; ii < array.length; ii++) {
+
+        // Get the value of the property in `itemToBecomeProperty`
         var specialProperty = array[ii][itemToBecomeProperty];
         if(specialProperty) {
+
+          // If a property on the overall object with that value doesn't exist create it as an array
           if(returnObject[specialProperty] === undefined) {
             returnObject[specialProperty] = [];
           }
+
+          // If the array item has it's `itemToBecomeProperty` value as an object property push this object into that property array
           returnObject[specialProperty].push(array[ii]);
         }
         
@@ -22,7 +32,8 @@ angular.module('meanbaseApp')
     };
 
 
-    // arrayToObjectWithObject() receives an array of objects and converts it into an object containing objects using the property name itemToBecomeProperty. For example let say we pass in this array and the itemToBecomeProperty is `contentName`: 
+    // ### Convert Array to Object
+    // helpers.arrayToObjectWithObject() receives an array of objects and converts it into an object containing objects using the property name itemToBecomeProperty. For example let say we pass in this array and the itemToBecomeProperty is `contentName`: 
 
     // **Before**
     // ```javascript
@@ -56,6 +67,8 @@ angular.module('meanbaseApp')
       return returnObject;
     };
 
+    // ### Convert Object to Array
+    // helpers.objectToArray() accepts an object and loops through it's properties and pushes their values into an array which is returned
     this.objectToArray = function(data) {
       if(!data || data === null || typeof data !== 'object') { return data; }
       var returnArray = [];
@@ -67,6 +80,8 @@ angular.module('meanbaseApp')
       return returnArray;
     };
 
+    // ### Loop through page extensions
+    // helpers.loopThroughPageExtensions() does a for loop through all the extensions on the current page and runs a callback function on each iteration passing in the current extension being iterated on
     this.loopThroughPageExtensions = function(fn) {
       for (var property in $rootScope.page.extensions) {
         if ($rootScope.page.extensions.hasOwnProperty(property)) {
@@ -78,6 +93,8 @@ angular.module('meanbaseApp')
       }
     };
 
+    // ### Check if object or array is empty
+    // helpers.isEmpty receives an object or array and returns true if it doesn't exist or has zero length or properties
     this.isEmpty = function (obj) {
       if(!obj) return true;
       if(Array.isArray(obj)) {
@@ -87,6 +104,8 @@ angular.module('meanbaseApp')
       }
     };
 
+    // ### Generate Select Drop Down Options
+    // helpers.generateSelectOptions() receives an array of strings which it converts into an array of objects, each containing a label and value property. It also allows you to add functions to filter what the values of those labels and values will be. This is currently only used on the CMS comments controller to help generate the list of pages that currently have comments on them.
     this.generateSelectOptions = function(model, labelFilter, valueFilter) {
       var modifiedModel = [];
       for (var i = 0; i < model.length; i++) {
@@ -112,7 +131,6 @@ angular.module('meanbaseApp')
 
     // `draggableGroups` is an object with properties representing the group name and having an array of objects that are in that group
     // They are in that format because we ran helpers.arrayToObjectWithArray so they would be easier to work with
-
     this.updatePositionData = function(draggableGroupsObject) {
       for(var group in draggableGroupsObject) {
         if (draggableGroupsObject.hasOwnProperty(group)) {
