46852b6fdf797a4249b4f872b36201e8a774c33f
Jon Paul <codingfriend1@gmail.com>
2015-07-01 19:24:20 -0500

    Removes exception logs. Fixes issue where winston logging would delete old debug logs.

--- server/components/logger/index.js
+++ server/components/logger/index.js
@@ -12,14 +12,20 @@ var logger = new (winston.Logger)({
     	prettyPrint: true,
     	// silent: false,
     }),
-    new winston.transports.DailyRotateFile({
-		  datePattern: '.yyyy-MM-ddTHH',
-		  filename: path.join(config.root, 'server', 'logs', 'debug.log'),
-		  json: false, 
-		  showLevel: false, 
-		  maxFiles: 5,
-		  prettyPrint: true
-		})
+  //   new winston.transports.DailyRotateFile({
+		//   datePattern: '.yyyy-MM-ddTHH',
+		//   filename: path.join(config.root, 'server', 'logs', 'debug.log'),
+		//   json: false, 
+		//   showLevel: false, 
+		//   maxFiles: 5,
+		//   prettyPrint: true
+		// })
+    new (winston.transports.File)({
+      filename: path.join(config.root, 'server', 'logs', 'debug.log'),
+      maxsize: 1000000, //1MB
+      maxFiles: 3,
+      prettyPrint: true
+    })
   ],
   // exceptionHandlers: [
   //   new winston.transports.DailyRotateFile({
