c81114248b6db44c480c716853c750e6e356c7fb
Jon Paul <codingfriend1@gmail.com>
2015-10-17 20:44:02 -0500

    Adds ability to remove groups of images by removing all the images from inside that group.

--- client/components/image-selector/image-selector.directive.js
+++ client/components/image-selector/image-selector.directive.js
@@ -39,12 +39,16 @@ angular.module('meanbaseApp')
         // only does this when directive is loaded
         function getGroups() {
           // Get media groups
+          scope.groups = ['all', 'selected'];
           for (var i = 0; i < scope.media.length; i++) { //Loop through each media
             for (var x = 0; x < scope.media[i].groups.length; x++) { //Loop through each group in media
               if(scope.groups.indexOf(scope.media[i].groups[x]) === -1) { //Already exists?
                 scope.groups.push(scope.media[i].groups[x]); //else add to groups array
               }
             }
+          }
+          if(scope.groups.indexOf(scope.selectedGroup) === -1) {
+            scope.selectedGroup = 'all';
           }
         }
 
@@ -416,6 +420,30 @@ angular.module('meanbaseApp')
 
         };
         getMedia();
+
+        scope.ungroupSelected = function() {
+
+          var urlArray = [];
+
+          // Get the visibile images' urls
+          for (var i = 0; i < scope.selectedImages.length; i++) {
+            urlArray.push(scope.selectedImages[i].url);
+          }
+
+          if(!scope.selectedGroup || scope.selectedGroup === 'all' || scope.selectedGroup === 'selected') { return false; }
+          if(urlArray.length < 1) { return false };
+
+          // Update those images
+          media.update({ url: {$in: urlArray } }, { $pull: {groups: scope.selectedGroup} }).then(function() {
+            for (var i = 0; i < scope.selectedImages.length; i++) {
+              if(scope.selectedImages[i].groups.indexOf(scope.selectedGroup) !== -1) {
+                scope.selectedImages[i].groups.splice(scope.selectedGroup, 1);
+              }
+            }
+            getGroups();
+          });
+
+        };
 
         // Clean up our event listeners when we leave this page
         scope.$on('$destroy', function() {
