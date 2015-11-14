3da1c1c05df2dbb23c981603c422582233004333
Jon Paul <codingfriend1@gmail.com>
2015-11-13 22:03:57 -0600

    Adds spec reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -62,7 +62,10 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DISABLE,
+
+    reporters: ['spec'],
+
 
 
     // enable / disable watching file and executing tests whenever any file changes
