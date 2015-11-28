214b8d0c07c0166be0b8bcf0ea6c16df754e3c7a
Jon Paul <codingfriend1@gmail.com>
2015-11-28 01:45:58 -0600

    Adds proper way to run terminal commands and adds ability to wipe dev mongodb database.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 /**
- * @overview Defines 
+ * @overview Defines
  * gulp install - Establishes app so its ready for development preview
  * 	Installs npm and bower.
  * 	Compiles jade into .tmp
  * 	Injects stylus files into app.styl
  * 	Compiles app.styl to .tmp
  * 	Injects vendor and app scripts and styles into server/views/index.html
- * 	
+ *
  * gulp compose
  * 	Does everything install does except for npm and bower
  * @author Jon Paul Miles <milesjonpaul@gmail.com>
@@ -38,6 +38,7 @@
 // 		htmlmin = require('gulp-htmlmin'),
 // 		debug = require('gulp-debug');
 
+var exec = require('child_process').exec;
 var gulp = require('gulp');
 var path = require('path');
 var fs = require('fs');
@@ -59,7 +60,16 @@ var config = {
 	    .filter(function(file) {
 	      return fs.statSync(path.join(dir, file)).isDirectory();
 	    });
-	}
+	},
+  runCommand: function(command) {
+    return function (cb) {
+      exec(command, function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+      });
+    }
+  }
 };
 
 var tasks = fs.readdirSync(path.join(__dirname, 'tasks')).filter(function(file) {
@@ -71,7 +81,7 @@ tasks.forEach(function (file) {
 });
 
 gulp.task('default', function() {
-	
+
 });
 
 gulp.task('compose', function() {
@@ -85,4 +95,8 @@ gulp.task('install', ['clean:tmp'], function() {
 			plugins.runSequence('jade', 'injectStylus', 'compileAppCSS', 'injectBowerComponents', 'injectComponents');
 		});
 	// });
+});
+
+gulp.task('empty-database', function() {
+  config.runCommand('mongo --eval "show dbs; use meanbase-dev; db.dropDatabase();"');
 });
