e2c76bfca6840b631e049e4a171da46a6c96d603
Jon Paul <codingfriend1@gmail.com>
2015-12-08 18:09:25 -0600

    Removes grunt from node_modules.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,22 @@
-cd ../
+sudo adduser meanbase-admin
+su meanbase-admin
+
+
+sudo apt-get update
+sudo apt-get install -y graphicsmagick
+sudo apt-get install -y nodejs
+npm update -g npm
+sudo apt-get install -y mongodb-org
+npm install -g bower
+npm install -g gulp
+npm install gulp-util --save-dev
+git clone https://github.com/codingfriend1/meanbase-1.0.0.git
+cd meanbase-1.0.0
+git checkout dev
+npm install -y
+gulp install
+mongod &
+fg %1
+
+
+npm install --save-dev orchestrator vinyl replace-ext chalk dateformat has-gulplog lodash.template lodash._reescape lodash._reevaluate
