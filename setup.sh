b19a37e9abfc286229a211bb7723b7f3e4db0398
Jon Paul <codingfriend1@gmail.com>
2015-12-08 18:34:49 -0600

    Removes create user from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-sudo adduser meanbase-admin
-su meanbase-admin
-
-
 sudo apt-get update
 sudo apt-get install -y graphicsmagick
 sudo apt-get install -y nodejs
@@ -9,11 +5,10 @@ npm update -g npm
 sudo apt-get install -y mongodb-org
 npm install -g bower
 npm install -g gulp
-npm install gulp-util --save-dev
 git clone https://github.com/codingfriend1/meanbase-1.0.0.git
 cd meanbase-1.0.0
 git checkout dev
-npm install -y
+npm install --verbose
 gulp install
 mongod &
 fg %1
