5649c284b75f3b2f58be8dd07828f2deb48517be
Jon Paul <codingfriend1@gmail.com>
2015-12-08 19:40:00 -0600

    Modifies setup.sh.

--- setup.sh
+++ setup.sh
@@ -5,13 +5,10 @@ npm update -g npm
 sudo apt-get install -y mongodb-org
 npm install -g bower
 npm install -g gulp
-git clone https://github.com/codingfriend1/meanbase-1.0.0.git
-cd meanbase-1.0.0
+#git clone https://github.com/codingfriend1/meanbase-1.0.0.git
+#cd meanbase-1.0.0
 git checkout dev
-npm install --verbose
+npm install
 gulp install
-mongod &
-fg %1
-
-
-npm install --save-dev orchestrator vinyl replace-ext chalk dateformat has-gulplog lodash.template lodash._reescape lodash._reevaluate
+#mongod &
+#fg %1
