cc7012146f28cf57092aef168c86655d0915ae42
Jon Paul <codingfriend1@gmail.com>
2015-12-09 23:44:57 -0600

    Adds sudo mongod and fixes all text editors showing up at once.

--- setup.sh
+++ setup.sh
@@ -58,10 +58,10 @@ gulp install
 gulp build
 cd dist/
 export NODE_ENV=production
-# mongod --smallfiles --fork --logpath /var/log/mongodb.log
+# sudo mongod --smallfiles --fork --logpath /var/log/mongodb.log
 # pm2 start server/app.js
 # sudo nginx
 
-#mongod --shutdown
+#sudo mongo 127.0.0.1/admin --eval "db.shutdownServer()"
 #pm2 stop all
 #sudo nginx -s quit
