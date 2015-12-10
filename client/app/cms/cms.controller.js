eafddd9dbcfd6e6f0e9056e0357a110f58a857e7
Jon Paul <codingfriend1@gmail.com>
2015-12-10 15:01:34 -0600

    Fixes annotation for cms.controller.

--- client/app/cms/cms.controller.js
+++ client/app/cms/cms.controller.js
@@ -1,55 +1,51 @@
 'use strict';
-(function(){
-	function CMSCtrl($scope, Auth, $rootScope, endpoints, api, $state) {
-		$scope.$parent.pageTitle = 'Manage Site';
-		$scope.user = Auth.getCurrentUser();
+angular.module('meanbaseApp').controller('cmsCtrl', function($scope, Auth, $rootScope, endpoints, api, $state) {
+	$scope.$parent.pageTitle = 'Manage Site';
+	$scope.user = Auth.getCurrentUser();
 
-		var states = $state.get();
-		$scope.cmsStates = [];
-		for (var i = 0; i < states.length; i++) {
-			if(states[i].name.indexOf('cms.') > -1) {
-				var state = angular.copy(states[i]);
-				if(!$scope.user.permissions || $scope.user.permissions.length === 0) {
-					state.userHasPermission = false;
-				} else {
-					state.userHasPermission = $scope.user.permissions.indexOf(state.hasPermission) > -1;
-				}
-				state.friendlyName = state.url.replace('/', '');
-				$scope.cmsStates.push(state);
+	var states = $state.get();
+	$scope.cmsStates = [];
+	for (var i = 0; i < states.length; i++) {
+		if(states[i].name.indexOf('cms.') > -1) {
+			var state = angular.copy(states[i]);
+			if(!$scope.user.permissions || $scope.user.permissions.length === 0) {
+				state.userHasPermission = false;
+			} else {
+				state.userHasPermission = $scope.user.permissions.indexOf(state.hasPermission) > -1;
 			}
- 		}
-
-		$scope.toggleMenu = function() {
-			$scope.menuOpen = !$scope.menuOpen;
-		};
-
-		$scope.pageTitle = 'Site Preferences';
-
-		if($scope.$parent.pageTitle) {
-		  document.title = $scope.$parent.pageTitle;
+			state.friendlyName = state.url.replace('/', '');
+			$scope.cmsStates.push(state);
+		}
 		}
 
-		$scope.isBanned = function(identifier) {
-			if(typeof identifier === 'object' || identifier) {
-				api.bannedMembers.find(identifier).success(function(response) {
-					console.log("response", response);
-				});
-			}
-		};
+	$scope.toggleMenu = function() {
+		$scope.menuOpen = !$scope.menuOpen;
+	};
 
-		$scope.isBanned({});
-		console.log("$scope.isBanned", $scope.isBanned);
+	$scope.pageTitle = 'Site Preferences';
+
+	if($scope.$parent.pageTitle) {
+	  document.title = $scope.$parent.pageTitle;
+	}
+
+	$scope.isBanned = function(identifier) {
+		if(typeof identifier === 'object' || identifier) {
+			api.bannedMembers.find(identifier).success(function(response) {
+				console.log("response", response);
+			});
+		}
+	};
+
+	$scope.isBanned({});
+	console.log("$scope.isBanned", $scope.isBanned);
 
 
-		$scope.ban = function(comment) {
-			console.log('comment', comment);
-			if(comment && comment.ip && comment.email) {
-				api.bannedMembers.create({ip: comment.ip, email: comment.email}).success(function(response) {
-					console.log("response", response);
-				});
-			}
-		};
-  }
-
-	angular.module('meanbaseApp').controller('cmsCtrl', CMSCtrl);
-})();
+	$scope.ban = function(comment) {
+		console.log('comment', comment);
+		if(comment && comment.ip && comment.email) {
+			api.bannedMembers.create({ip: comment.ip, email: comment.email}).success(function(response) {
+				console.log("response", response);
+			});
+		}
+	};
+});
