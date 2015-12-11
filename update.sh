4a3025d5ebcdf694aac0187eb1a00b7350094591
Jon Paul <codingfriend1@gmail.com>
2015-12-10 23:57:17 -0600

    Removes cd dist/ from update.sh.

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ pm2 stop all; sudo mongo 127.0.0.1/admin --eval "db.shutdownServer()"; sudo ngin
 # Grab Updates
 git pull
 gulp build
-cd dist
 
 # Start server
-sudo mongod --smallfiles --fork --logpath /var/log/mongodb.log; pm2 start server/app.js; sudo nginx
+sudo mongod --smallfiles --fork --logpath /var/log/mongodb.log; pm2 start dist/server/app.js; sudo nginx
