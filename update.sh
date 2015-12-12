617678c28a2cb240ef84b30693e17a2dec54d175
Jon Paul <codingfriend1@gmail.com>
2015-12-12 13:40:54 -0600

    Updates update.sh.

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 # Stop server
-export NODE_ENV=production
 pm2 stop all; sudo mongo 127.0.0.1/admin --eval "db.shutdownServer()"; sudo nginx -s quit
 
 # Grab Updates
 git pull
+npm install
+bower install
 gulp build
 
+export NODE_ENV=production
 # Start server
 sudo mongod --smallfiles --fork --logpath /var/log/mongodb.log; pm2 start dist/server/app.js; sudo nginx
