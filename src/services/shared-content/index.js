16af37eeaa234bda4f2bcdec29912a51b00ff0c7
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-08 23:27:05 -0500

    Changes lean to true on shared content.

--- src/services/shared-content/index.js
+++ src/services/shared-content/index.js
@@ -9,10 +9,7 @@ module.exports = function() {
 
   const options = {
     Model: sharedContent,
-    paginate: {
-      default: 5,
-      max: 25
-    }
+    lean: true
   };
 
   // Initialize our service with any options it requires
