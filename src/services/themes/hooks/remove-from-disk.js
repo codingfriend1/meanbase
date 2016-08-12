8de522eab116fc2c9c61f97f12f28912b1bc3138
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-12 16:31:51 -0500

    Updates error message for deleting themes from disk.

--- src/services/themes/hooks/remove-from-disk.js
+++ src/services/themes/hooks/remove-from-disk.js
@@ -6,7 +6,7 @@ export default options => {
     for (var i = 0; i < hook.result.length; i++) {
       const theme = hook.result[i];
       if(!hook.app.get('themesPath')) {
-        return new Error('Sorry client path is missing');
+        return new Error('Sorry themesPath is missing');
       }
       const folderName = path.join(hook.app.get('themesPath'), theme.url || 'al23gl239');
       fse.remove(folderName);
