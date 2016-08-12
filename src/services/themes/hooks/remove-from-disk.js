ebd667d3af4bf4db34af1f700aeeafc88d25a0ee
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-12 16:29:55 -0500

    Adds beginning of deleting themes.

---
+++ src/services/themes/hooks/remove-from-disk.js
@@ -0,0 +1,16 @@
+import fse from 'fs-extra';
+import path from 'path';
+
+export default options => {
+  return hook => {
+    for (var i = 0; i < hook.result.length; i++) {
+      const theme = hook.result[i];
+      if(!hook.app.get('themesPath')) {
+        return new Error('Sorry client path is missing');
+      }
+      const folderName = path.join(hook.app.get('themesPath'), theme.url || 'al23gl239');
+      fse.remove(folderName);
+    }
+    return hook;
+  }
+}
