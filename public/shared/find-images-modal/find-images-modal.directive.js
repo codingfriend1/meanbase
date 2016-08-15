59e733e7f8f13f4df29ed864ca3317457cea3b35
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-15 12:11:27 -0500

    Adds enable and disable click along with attaching the click event specifically to image if mb-src exists.

--- public/shared/find-images-modal/find-images-modal.directive.js
+++ public/shared/find-images-modal/find-images-modal.directive.js
@@ -8,9 +8,17 @@ angular.module('meanbaseApp')
       scope: true,
       link: function (scope, element, attrs) {
       	var config = scope.findImagesConfig;
+        var chosenElement = element;
 
-      	element.bind('click', function() {
+        if(element.find('mb-src')) {
+          chosenElement = element.find('[mb-src]:first');
+        }
+
+      	chosenElement.bind('click', function(event) {
           if(!$rootScope.editMode) { return false; }
+
+          if (event.target !== this) { return };
+
       		// openImageModal is defined in main.controller
       		$rootScope.openImageModal(config, function(selectedImages) {
       			$rootScope.$emit('cms.choseImages', {gallerySlug:  config.gallerySlug, images: selectedImages});
