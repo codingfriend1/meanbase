29fd85b1b8c71cec1def2838ef4faea1c9fc2dc5
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-16 17:51:36 -0500

    Adds add button to page lists in green floating button.

---
+++ public/shared/mb-list-add/mbListAdd.directive.js
@@ -0,0 +1,33 @@
+angular.module('meanbaseApp')
+  .directive('mbListAdd', function ($rootScope, endpoints, $timeout) {
+    return {
+      template: '<div class="add-to-list-btn"><i class="fa fa-plus fa-lg"></i></div>',
+      restrict: 'EA',
+      scope: {
+        list: '=',
+        item: '='
+      },
+      link: function (scope, element, attrs) {
+
+        if(!scope.list) {
+          scope.list = [];
+        }
+
+        if(!scope.item) {
+          scope.item = {};
+        }
+
+        element.bind('click', function(event) {
+          $rootScope.$emit('cms.saveListItem');
+          $timeout(function() {
+            $timeout(function() {
+              scope.list.push(_.cloneDeep(scope.item));
+              scope.item = {};
+
+            });
+          });
+        });
+      }
+    }
+
+  });
