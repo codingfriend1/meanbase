2e35ca9013844efd414f4e21cb4a5623f69bd1f4
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-16 13:28:29 -0500

    In choose icon adds mechanism to protect user from selecting an invisible icon.

--- public/shared/choose-icon/chooseIcon.directive.js
+++ public/shared/choose-icon/chooseIcon.directive.js
@@ -1,28 +1,29 @@
 angular.module('meanbaseApp')
   .directive('chooseIcon', function ($rootScope, endpoints, $compile) {
     return {
-      template: '<i ng-class="for[property].classes" ng-click="handleIconClick($event, for, property, for[property].url)"></i>',
+      template: '<i ng-class="belongsTo[property].classes" ng-click="handleIconClick($event, belongsTo, property, belongsTo[property].url)"></i>',
       restrict: 'EA',
       scope: true,
       replace: true,
       link: function (scope, element, attrs) {
-        scope.for = scope.$parent.$eval(attrs.for);
+        scope.belongsTo = scope.$parent.$eval(attrs.belongsTo);
 
-        if(!scope.for) { scope.for = {}; }
+        if(!scope.belongsTo) { scope.belongsTo = {}; }
+        scope.property = attrs.property;
 
         function setDefaultIfEmpty() {
-          if(!scope.for[attrs.property]  || _.isEmpty(scope.for[attrs.property])) {
-            scope.for[attrs.property] = {
+          if(!scope.belongsTo[attrs.property]  || _.isEmpty(scope.belongsTo[attrs.property])) {
+            scope.belongsTo[attrs.property] = {
               classes: 'fa fa-pencil fa-2x erase-this'
             };
-          } else if(scope.for[attrs.property].classes === '') {
-            scope.for[attrs.property].classes = 'fa fa-pencil fa-2x erase-this';
+          } else if(scope.belongsTo[attrs.property].classes === '') {
+            scope.belongsTo[attrs.property].classes = 'fa fa-pencil fa-2x erase-this';
           }
         }
 
         function removeDefault() {
-          if(scope.for[attrs.property].classes === 'fa fa-pencil fa-2x erase-this') {
-            scope.for[attrs.property].classes = '';
+          if(scope.belongsTo[attrs.property].classes === 'fa fa-pencil fa-2x erase-this') {
+            scope.belongsTo[attrs.property].classes = '';
           }
         }
 
@@ -32,7 +33,7 @@ angular.module('meanbaseApp')
 
         scope.$onRootScope('cms.editMode', function(event, value) {
           if(value) {
-            setDefaultIfEmpty()
+            setDefaultIfEmpty();
           } else {
             removeDefault();
           }
