d4f1b2f055ad28b4ff5d8318642a611e268f180b
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-24 15:58:23 -0500

    Adds material design upload buttons.

--- public/themes/meanbase-demo/index.js
+++ public/themes/meanbase-demo/index.js
@@ -2,17 +2,17 @@
 import "./js/selling-point-list/selling-point-list.html";
 import "./js/selling-point-list/toggle-type.modal.html";
 import "./js/bullet-point-list/bullet-point-list.html";
-import "./templates/blog/blog-template.html";
 import "./templates/archive/archive-template.html";
+import "./templates/blog/blog-template.html";
 import "./templates/home/home-template.html";
 // end inject jade
 
 // inject js
+import "./js/selling-point-list/selling-point-list.directive.js";
 import "./templates/home/home.controller.js";
+import "./js/bullet-point-list/bullet-point-list.directive.js";
 import "./templates/blog/blog.controller.js";
 import "./templates/archive/archive.controller.js";
-import "./js/selling-point-list/selling-point-list.directive.js";
-import "./js/bullet-point-list/bullet-point-list.directive.js";
 // end inject js
 
 // inject stylus
