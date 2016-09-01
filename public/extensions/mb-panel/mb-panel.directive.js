e451477b35c1f8902a46fb1b8d57cd75557ae0fd
Jon Paul Miles <codingfriend1@gmail.com>
2016-08-31 23:53:40 -0500

    Fixes choose extension modal.

--- public/extensions/mb-panel/mb-panel.directive.js
+++ public/extensions/mb-panel/mb-panel.directive.js
@@ -5,34 +5,37 @@ angular.module('meanbaseApp').directive('mbPanel', (api, $rootScope, $timeout) =
 
     scope.data = {}
     let alreadyHasData = false
-    let alreadyHasAutoSaveData = false
+    let alreadyHasAutoSaveData = false;
 
-    try {
-      let extensionData = await api.custom.find({belongsTo: scope.listItem.label, key: scope.listItem.key})
-      extensionData = extensionData[0]
-      if(extensionData) {
-        alreadyHasData = true
-        console.log('setting extension data');
-        scope.data = extensionData.value
+    (async () => {
+      try {
+        let extensionData = await api.custom.find({belongsTo: scope.listItem.label, key: scope.listItem.key})
+        extensionData = extensionData[0]
+        if(extensionData) {
+          alreadyHasData = true
+          scope.data = extensionData.value
+        }
+      } catch(err) {
+        console.log(`Error fetching ${scope.listItem.key} data`, err);
       }
-    } catch(err) {
-      console.log(`Error fetching ${scope.listItem.key} data`, err);
-    }
+    })();
+
+    scope.data = scope.listItem.data
 
 
     if(!$rootScope.currentUser) { return false }
 
-    $timeout(async () => {
-      api.staging.find({belongsTo: scope.listItem.label, key: scope.listItem.key}).then(function(stagingData) {
-        stagingData = stagingData[0]
-        if(stagingData) {
-          if($rootScope.editMode) {
-            scope.data = stagingData.data
-          }
-          alreadyHasAutoSaveData = true
-        }
-      });
-    });
+    // $timeout(async () => {
+    //   api.staging.find({belongsTo: scope.listItem.label, key: scope.listItem.key}).then(function(stagingData) {
+    //     stagingData = stagingData[0]
+    //     if(stagingData) {
+    //       if($rootScope.editMode) {
+    //         scope.data = stagingData.data
+    //       }
+    //       alreadyHasAutoSaveData = true
+    //     }
+    //   });
+    // });
 
     async function saveEdits(event) {
       try {
@@ -46,51 +49,70 @@ angular.module('meanbaseApp').directive('mbPanel', (api, $rootScope, $timeout) =
         }
         scope.data = response.value
 
-        removeAutoSaveData()
+        scope.listItem.data = { items: [] }
 
       } catch(err) {
         console.log('Error saving extension data ', err);
       }
 
     }
-
-    async function removeAutoSaveData() {
-      try {
-        await api.staging.delete({belongsTo: scope.listItem.label, key: scope.listItem.key})
-        alreadyHasAutoSaveData = false
-      } catch(err) {
-        console.log(`Error deleting ${scope.listItem.label} autosave data`, err);
-      }
-    }
-
-    async function fetchAutoSaveData() {
-      try {
-        let data = await api.staging.find({belongsTo: scope.listItem.label, key: scope.listItem.key})
-        data = data[0]
-        if(data) {
-          scope.data = data.data
-          alreadyHasAutoSaveData = true
-        }
-      } catch(err) {
-        console.log(`Error deleting ${scope.listItem.label} autosave data`, err);
-      }
-    }
-
-
-    scope.autoSaveEdits = async function(event) {
-      try {
-        let response
-        if(alreadyHasAutoSaveData) {
-          response = await api.staging.update({belongsTo: scope.listItem.label, key: scope.listItem.key}, {data: scope.data})
-        } else {
-          alreadyHasAutoSaveData = true
-          response = await api.staging.create({belongsTo: scope.listItem.label, key: scope.listItem.key, data: scope.data, enabled: true, permission: 'editContent'})
-        }
-      } catch(err) {
-        console.log('Error saving extension data ', err);
-      }
-
-    }
+    // async function saveEdits(event) {
+    //   try {
+    //     let response
+    //     if(alreadyHasData) {
+    //       response = await api.custom.update({belongsTo: scope.listItem.label, key: scope.listItem.key}, {value: scope.data})
+    //     } else {
+    //       alreadyHasData = true
+    //       response = await api.custom.create({belongsTo: scope.listItem.label, key: scope.listItem.key, value: scope.data, enabled: true, permission: 'editContent'})
+    //
+    //     }
+    //     scope.data = response.value
+    //
+    //     removeAutoSaveData()
+    //
+    //   } catch(err) {
+    //     console.log('Error saving extension data ', err);
+    //   }
+    //
+    // }
+    //
+    // async function removeAutoSaveData() {
+    //   try {
+    //     await api.staging.delete({belongsTo: scope.listItem.label, key: scope.listItem.key})
+    //     alreadyHasAutoSaveData = false
+    //   } catch(err) {
+    //     console.log(`Error deleting ${scope.listItem.label} autosave data`, err);
+    //   }
+    // }
+    //
+    // async function fetchAutoSaveData() {
+    //   try {
+    //     let data = await api.staging.find({belongsTo: scope.listItem.label, key: scope.listItem.key})
+    //     data = data[0]
+    //     if(data) {
+    //       scope.data = data.data
+    //       alreadyHasAutoSaveData = true
+    //     }
+    //   } catch(err) {
+    //     console.log(`Error deleting ${scope.listItem.label} autosave data`, err);
+    //   }
+    // }
+    //
+    //
+    // scope.autoSaveEdits = async function(event) {
+    //   try {
+    //     let response
+    //     if(alreadyHasAutoSaveData) {
+    //       response = await api.staging.update({belongsTo: scope.listItem.label, key: scope.listItem.key}, {data: scope.data})
+    //     } else {
+    //       alreadyHasAutoSaveData = true
+    //       response = await api.staging.create({belongsTo: scope.listItem.label, key: scope.listItem.key, data: scope.data, enabled: true, permission: 'editContent'})
+    //     }
+    //   } catch(err) {
+    //     console.log('Error saving extension data ', err);
+    //   }
+    //
+    // }
 
     // let directiveWatcher = scope.$watch(scope.data, _.debounce(function(newValue, oldValue) {
     //   if(typeof newValue !== oldValue) {
@@ -106,14 +128,14 @@ angular.module('meanbaseApp').directive('mbPanel', (api, $rootScope, $timeout) =
     let publishListener = scope.$onRootScope('cms.publish', saveEdits)
     let publishChangesListener = scope.$onRootScope('cms.publishChanges', saveEdits)
     // scope.$onRootScope('cms.autoSave', _.debounce(autoSaveEdits, 100))
-    let resetDraftListener = scope.$onRootScope('cms.resetDraft', removeAutoSaveData)
-    let pullAutoSaveListener = scope.$onRootScope('cms.pullAutoSaveData', fetchAutoSaveData)
+    // let resetDraftListener = scope.$onRootScope('cms.resetDraft', removeAutoSaveData)
+    // let pullAutoSaveListener = scope.$onRootScope('cms.pullAutoSaveData', fetchAutoSaveData)
 
     scope.$destroy(function() {
       publishListener()
       publishChangesListener()
-      resetDraftListener()
-      pullAutoSaveListener()
+      // resetDraftListener()
+      // pullAutoSaveListener()
     })
 
   }
