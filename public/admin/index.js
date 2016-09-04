f7971a3a0f0c9ef0b0d1b21c5bb9af6398605bfe
Jon Paul Miles <codingfriend1@gmail.com>
2016-09-04 18:39:24 -0500

    Removes padding from headbar.

--- public/admin/index.js
+++ public/admin/index.js
@@ -24,8 +24,8 @@ import "./code/components/dialog/dialog.directive.js";
 import "./code/components/date-picker/date-picker.directive.js";
 import "./code/components/crud/crud.service.js";
 import "./code/components/camel-to-human/camel-to-human.filter.js";
-import "./code/account/login/login.controller.js";
 import "./code/account/settings/settings.controller.js";
+import "./code/account/login/login.controller.js";
 import "./code/users/users.js";
 import "./code/users/users.controller.js";
 import "./code/themes/themes.js";
@@ -43,10 +43,10 @@ import "./code/comments/comments.js";
 import "./code/comments/comments.controller.js";
 import "./code/cms/cms.js";
 import "./code/cms/cms.controller.js";
-import "./code/account/account.js";
 import "./code/analytics/google-analytics-embed-customizations.js";
 import "./code/analytics/analytics.js";
 import "./code/analytics/analytics.controller.js";
+import "./code/account/account.js";
 // end inject js
 
 // inject jade
@@ -56,8 +56,8 @@ import "./code/comments/comments.jade";
 import "./code/extensions/extensions.jade";
 import "./code/import/import.jade";
 import "./code/media/media.jade";
-import "./code/themes/themes.jade";
 import "./code/pages/pages.jade";
+import "./code/themes/themes.jade";
 import "./code/users/users.jade";
 import "./code/account/login/login.jade";
 import "./code/account/settings/settings.jade";
