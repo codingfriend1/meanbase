871c453fa158059e69616c1a999ec11ffd8504f7
Jon Paul Miles <codingfriend1@gmail.com>
2016-09-05 10:19:54 -0500

    Adds navigation drawer for headbar.

--- public/app/components/cms.headbar/cms.headbar.controller.js
+++ public/app/components/cms.headbar/cms.headbar.controller.js
@@ -1,10 +1,35 @@
 (function(){
+
 	angular.module('meanbaseApp').controller('cms.headbar.controller', HeadbarController);
 
 	// @ngInject
 	function HeadbarController($scope, $rootScope, endpoints, $state, $location, $modal, $timeout, helpers, toastr, api) {
 
     window.dropdown.init()
+
+    $scope.drawerPages = []
+
+    let mbPageDrawer = document.getElementById("mb-pages-drawer")
+    /* Set the width of the side navigation to 250px */
+    this.openMBPageDrawer = function($event) {
+      if(mbPageDrawer) {
+        mbPageDrawer.classList.add('mb-drawer-open')
+      }
+
+      $event.stopPropagation()
+
+      document.body.addEventListener('click', function mbCloseDrawer(event) {
+        if(!$(event.target).closest("#mb-pages-drawer").length || $(event.target).is('.mb-page-link')) {
+          mbPageDrawer.classList.remove('mb-drawer-open')
+          document.body.removeEventListener('click', mbCloseDrawer)
+        }
+        event.stopPropagation()
+      })
+
+      api.pages.find({$select: ['url', 'title']}).then(function(response) {
+        $scope.drawerPages = response
+      });
+    }
 
     const msTillAutoSaveMenus = 100;
     const msTillAutoSavePage = 100;
