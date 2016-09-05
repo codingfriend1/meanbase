bd34245651ab6379532950570899abeeb1dcc05d
Jon Paul Miles <codingfriend1@gmail.com>
2016-09-04 23:20:06 -0500

    Changes search addons to filter add-ons.

--- public/themes/small-business-portfolio/index.js
+++ public/themes/small-business-portfolio/index.js
@@ -1,6 +1,6 @@
 // inject jade
-import "./templates/about/about-template.html";
 import "./templates/front-page/font-page-template.html";
+import "./templates/about/about-template.html";
 import "./templates/portfolio-item/portfolio-template.html";
 // end inject jade
 
@@ -9,8 +9,8 @@ import "./assets/js/small-business-portfolio.js";
 // end inject js
 
 // inject stylus
-import "./assets/css/small-business-portfolio.styl";
 import "./templates/about/round-about.styl";
 import "./templates/front-page/business-frontpage.styl";
 import "./templates/portfolio-item/portfolio-item.styl";
+import "./assets/css/small-business-portfolio.styl";
 // end inject stylus
