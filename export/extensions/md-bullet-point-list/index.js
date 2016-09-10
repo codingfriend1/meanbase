5c6fa3473585e0bb9393d2b46125667f2f9e2852
Jon Paul Miles <codingfriend1@gmail.com>
2016-09-10 00:08:40 -0500

    Adds small change.

---
+++ export/extensions/md-bullet-point-list/index.js
@@ -0,0 +1,11 @@
+// inject jade
+import "./bullet-point-list-extension.html";
+// end inject jade
+
+// inject js
+// end inject js
+
+// inject stylus
+// end inject stylus
+
+// It works
---
+++ export/extensions/md-selling-point-list/selling-point-list.directive.js
@@ -0,0 +1,31 @@
+angular.module('meanbaseApp')
+  .directive('sellingPointList', function ($rootScope, endpoints, $compile, $timeout, $modal) {
+    return {
+      templateUrl: require('./selling-point-list.html'),
+      restrict: 'EA',
+      replace: true,
+      link: function (scope, element, attrs) {
+        scope.point = {};
+
+      	scope.openModal = function (point) {
+      	  var modalInstance = $modal.open({
+            templateUrl: require('./toggle-type.modal.html'),
+      	    controller: function menuModal($scope, $modalInstance, point) {
+              if(!point.left) { point.left = false; }
+              $scope.left = point.left.toString();
+              $scope.chooseLeft = function(left) {
+                point.left = left === 'true';
+                $modalInstance.dismiss();
+              };
+            },
+            resolve: {
+              point: function() {
+                return point;
+              }
+            },
+      	    size: 'md'
+      	  });
+        };
+      }
+    }
+  });
